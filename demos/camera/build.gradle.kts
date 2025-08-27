5d52a623c8aa39f91d34c83aa844e2461e3b2b34
dimadesu <dimadesu@gmail.com>
2025-08-27 12:56:00 +1000

    Add exoplayer dependencies

--- demos/camera/build.gradle.kts
+++ demos/camera/build.gradle.kts
@@ -62,6 +62,10 @@ dependencies {
     implementation(libs.androidx.preference.ktx)
     implementation(libs.androidx.datastore.preferences)
     implementation(libs.androidx.datastore)
+    implementation("androidx.media3:media3-exoplayer:1.8.0")
+    // ExoPlayer UI (optional, for PlayerView)
+    implementation("androidx.media3:media3-ui:1.8.0")
+    implementation("androidx.media3:media3-datasource-rtmp:1.8.0")
 
     testImplementation(libs.junit)
 
